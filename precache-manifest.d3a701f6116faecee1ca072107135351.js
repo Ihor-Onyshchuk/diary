b89b599fc1e58dc8f15c7ed4e6084410dda674bd
ihor <onyshchukihor11@gmail.com>
2020-07-16 15:04:58 +0300

    Updates

---
+++ precache-manifest.d3a701f6116faecee1ca072107135351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd1307633987a527a9fa0a8654f48ab",
+    "url": "/diary/index.html"
+  },
+  {
+    "revision": "02f8e77c340a97653458",
+    "url": "/diary/static/css/main.55aa60e8.chunk.css"
+  },
+  {
+    "revision": "5b683f96798d8416e1f8",
+    "url": "/diary/static/js/2.adeb89f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/diary/static/js/2.adeb89f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02f8e77c340a97653458",
+    "url": "/diary/static/js/main.5ed922dd.chunk.js"
+  },
+  {
+    "revision": "7945922d61a03962dd67",
+    "url": "/diary/static/js/runtime-main.27ab6701.js"
+  }
+]);
